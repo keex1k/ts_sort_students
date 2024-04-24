6c934df72c42100dd14c54aa5b88e80bcd8983f7
Bartek <kowalski_1410@wp.pl>
2024-04-24 21:44:31 +0200

    deleted if-else

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -22,61 +22,49 @@ export function sortStudents(students: Student[],
   order: SortOrder): Object[] {
   const result = [...students];
 
-  if (order === 'asc') {
-    switch (sortBy) {
-      case SortType.Name:
-        return result.sort((a, b) => a.name.localeCompare(b.name));
-      case SortType.Surname:
-        return result.sort((a, b) => a.surname.localeCompare(b.surname));
-      case SortType.AverageGrade:
-        return result.sort((a, b) => {
-          const sumaA: number = a.grades.reduce((x, y) => {
-            return x + y;
-          }, 0);
-          const sumaB: number = b.grades.reduce((x, y) => {
-            return x + y;
-          }, 0);
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return result.sort((a, b) => {
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+      });
+    case SortType.AverageGrade:
+      return result.sort((a, b) => {
+        const sumaA: number = a.grades.reduce((x, y) => {
+          return x + y;
+        }, 0);
+        const sumaB: number = b.grades.reduce((x, y) => {
+          return x + y;
+        }, 0);
 
-          const avgA: number = sumaA / a.grades.length;
-          const avgB: number = sumaB / b.grades.length;
+        const avgA: number = sumaA / a.grades.length;
+        const avgB: number = sumaB / b.grades.length;
 
-          return avgA - avgB;
-        });
-      default:
-        return [];
-    }
-  } else {
-    switch (sortBy) {
-      case SortType.Age:
-        return result.sort((a, b) => b.age - a.age);
-      case SortType.Married:
-        return result.sort((a, b) => {
-          if (a.married === b.married) {
-            return 0;
-          }
+        return order === 'asc'
+          ? avgA - avgB
+          : avgB - avgA;
+      });
+    case SortType.Married:
+      return result.sort((a, b) => {
+        if (a.married === b.married) {
+          return 0;
+        }
 
-          if (a.married) {
-            return -1;
-          }
+        if (a.married) {
+          return order === 'asc' ? 1 : -1;
+        }
 
-          return 1;
-        });
-      case SortType.AverageGrade:
-        return result.sort((a, b) => {
-          const sumaA: number = a.grades.reduce((x, y) => {
-            return x + y;
-          }, 0);
-          const sumaB: number = b.grades.reduce((x, y) => {
-            return x + y;
-          }, 0);
-
-          const avgA: number = sumaA / a.grades.length;
-          const avgB: number = sumaB / b.grades.length;
-
-          return avgB - avgA;
-        });
-      default:
-        return [];
-    }
+        return order === 'asc' ? -1 : 1;
+      });
+    case SortType.Age:
+      return result.sort((a, b) => {
+        return order === 'asc'
+          ? a.age - b.age
+          : b.age - a.age;
+      });
+    default:
+      return [];
   }
 }
